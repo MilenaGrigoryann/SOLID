af2e8347ab2cbe3ec7cb71dcd74d3c3943c53e17
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-22 23:23:08 +0400

    Add files via upload

---
+++ OCP/IReportGeneration.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace OCP
+{
+    public class IReportGeneration
+    {
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public virtual void GenerateReport(Employee em)
+        {
+            // From base
+        }
+    }
+    /// <summary>
+    /// Class to generate Crystal report
+    /// </summary>
+    public class CrystalReportGeneraion : IReportGeneration
+    {
+        public override void GenerateReport(Employee em)
+        {
+            Console.WriteLine("Generate Crystal report");
+        }
+    }
+    /// <summary>
+    /// Class to generate PDF report
+    /// </summary>
+    public class PDFReportGeneraion : IReportGeneration
+    {
+        public override void GenerateReport(Employee em)
+        {
+            Console.WriteLine("Generate PDF report");
+        }
+    }
+
+}
---
+++ OCP/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace OCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Employee employee = new Employee { Employee_Id = 1234, Employee_Name = "Ann" };
+            IReportGeneration reporter = new CrystalReportGeneraion();
+            employee.InsertIntoEmployeeTable(employee);
+            reporter.GenerateReport(employee);
+        }
+    }
+}
