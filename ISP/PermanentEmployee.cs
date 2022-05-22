5ba2fa373d1671bf59ddb4acf53c11d0223df8cb
MilenaGrigoryann <101062835+MilenaGrigoryann@users.noreply.github.com>
2022-05-22 23:24:53 +0400

    Add files via upload

---
+++ ISP/PermanentEmployee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISP
+{
+    public class PermanentEmployee : IAddOperation, IGetOperation
+    {
+        public bool AddEmployeeDetails()
+        {
+            Console.WriteLine("Adding Employee Details");
+            return true;
+        }
+
+        public bool ShowEmployeeDetails(int employeeId)
+        {
+            Console.WriteLine("Showing Employee Details");
+            return true;
+        }
+    }
+}
